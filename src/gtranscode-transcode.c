69749ff710e820a66073309cdd36a8b87dfb9f15
David Newgas <david@newgas.net>
2008-01-28 01:55:43 +0000

    Seperated out files It works!

---
+++ src/gtranscode-transcode.c
@@ -0,0 +1,154 @@
+/*
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#define DEBUG TRUE
+
+#include <gst/gst.h>
+#include <glib.h>
+#include <gnome.h>
+#ifdef DEBUG
+#include <glib/gstdio.h>
+#endif
+
+#include "../include/gtranscode.h"
+
+GstElement *pipeline;
+
+void
+gtranscode_object_set (gchar ** option_pair, gpointer object)
+{
+  g_object_set (G_OBJECT (object), option_pair[0], option_pair[1], NULL);
+}
+
+void
+pad_added (GstElement
+	   * src, GstPad * src_pad, gboolean arg1, GstElement * sink)
+{
+  GstPad *sink_pad;
+  GstPadLinkReturn success = GST_PAD_LINK_OK;
+  sink_pad = gst_element_get_pad (sink, "sink");
+#ifdef DEBUG
+  g_printf ("Pad added signal called, is it compatible? ");
+#endif
+
+  if (gst_pad_can_link (src_pad, sink_pad))
+    {
+#ifdef DEBUG
+      g_printf ("yes");
+#endif
+      success = gst_pad_link (src_pad, sink_pad);
+    }
+#ifdef DEBUG
+  g_printf ("\n");
+
+  gst_object_unref (sink_pad);
+  switch (success)
+	  {
+	  case GST_PAD_LINK_WRONG_HIERARCHY:
+		  g_printf("Fail: Wrong Hierachy\n");
+  		break;
+  case GST_PAD_LINK_WAS_LINKED:
+		  g_printf("Fail: already linked\n");
+  		break;
+  case GST_PAD_LINK_WRONG_DIRECTION:
+		  g_printf("Fail: wrong direction\n");
+  		break;
+  case GST_PAD_LINK_NOFORMAT:
+		  g_printf("Fail: no common format\n");
+  		break;
+  case GST_PAD_LINK_NOSCHED:
+  		break;
+		  g_printf("Fail: can't cooperate\n");
+  		break;
+  case GST_PAD_LINK_REFUSED:
+		  g_printf("Fail: other reason\n");
+  		break;
+  case GST_PAD_LINK_OK:
+	  break;
+	  }
+#endif
+  return;
+}
+
+void
+  transcode
+  (GstElementFactory *
+   source_factory,
+   GList * source_opts,
+   GstElementFactory *
+   container_factory,
+   GList *
+   container_opts,
+   GstElementFactory *
+   audio_codec_factory,
+   GList *
+   audio_codec_opts,
+   GstElementFactory *
+   video_codec_factory,
+   GList *
+   video_codec_opts, GstElementFactory * sink_factory, GList * sink_opts)
+{
+  GstElement *source, *container, *video_codec, *audio_convert, *audio_codec, *sink, *decoder;
+  /* Set up gst elements */
+  pipeline = gst_pipeline_new ("transcoder");
+  source = gst_element_factory_create (source_factory, "source");
+  decoder = gst_element_factory_make ("decodebin2", "decoder");
+  container = gst_element_factory_create (container_factory, "muxer");
+  video_codec =
+    gst_element_factory_create (video_codec_factory, "video_codec");
+  audio_convert = gst_element_factory_make ("audioconvert" , "audio_convert");
+  audio_codec =
+    gst_element_factory_create (audio_codec_factory, "audio_codec");
+  sink = gst_element_factory_create (sink_factory, "sink");
+  if (!pipeline || !audio_convert
+      || !source || !container || !video_codec || !audio_codec || !sink)
+    {
+      g_printerr ("Could not initialise elements!\n");
+    }
+
+  g_list_foreach (source_opts, (GFunc) gtranscode_object_set, source);
+  g_list_foreach (container_opts, (GFunc) gtranscode_object_set, container);
+  g_list_foreach
+    (video_codec_opts, (GFunc) gtranscode_object_set, video_codec);
+  g_list_foreach
+    (audio_codec_opts, (GFunc) gtranscode_object_set, audio_codec);
+  g_list_foreach (sink_opts, (GFunc) gtranscode_object_set, sink);
+  gst_bin_add_many
+    (GST_BIN (pipeline),audio_convert,
+     source, decoder, video_codec, audio_codec, container, sink, NULL);
+  gst_element_link_many (video_codec, container, sink, NULL);
+  gst_element_link_many (audio_convert, audio_codec, container, NULL);
+  gst_element_link_many (source, decoder, NULL);
+  g_signal_connect
+    (decoder, "new-decoded-pad", G_CALLBACK (pad_added), video_codec);
+  g_signal_connect
+    (decoder, "new-decoded-pad", G_CALLBACK (pad_added), audio_convert);
+  /* Set up events for when it's done */
+  /* Set up progress indicator */
+  /* Start pipline */
+#ifdef DEBUG
+  g_printf ("Starting Transcode.\n");
+#endif
+  gst_element_set_state (pipeline, GST_STATE_PLAYING);
+#ifdef DEBUG
+  g_printf ("Printing pipline.\n");
+  gst_xml_write_file (pipeline, g_fopen ("/tmp/out.xml", "w"));
+#endif
+}
--- src/main.c
+++ src/main.c
@@ -13,64 +13,69 @@
 #include <glib.h>
 #include <glib/gstdio.h>
 
-void
-gtranscode_object_set (gchar ** option_pair, gpointer object)
+#include "../include/gtranscode.h"
+
+
+GList *sources = NULL, *containers = NULL;
+
+GstElement * pipeline;
+
+GladeXML *xml;
+
+GtranscodeElementFactory *
+gtranscode_element_factory_find (gchar * identifier)
 {
-  g_object_set (G_OBJECT (object), option_pair[0], option_pair[1], NULL);
+  GtranscodeElementFactory *result = g_new (GtranscodeElementFactory, 1);
+  result->ElementFactory = gst_element_factory_find (identifier);
+  result->name = gst_element_factory_get_longname (result->ElementFactory);
+  result->allowed_audio_codecs = NULL;
+  result->allowed_video_codecs = NULL;
+  return result;
+}
+
+GtranscodeElementFactory *
+gtranscode_element_factory_find_with_children (gchar * identifier)
+{
+  GtranscodeElementFactory *result =
+    gtranscode_element_factory_find (identifier);
+  result->allowed_audio_codecs =
+    g_list_append (result->allowed_audio_codecs,
+		   gtranscode_element_factory_find ("vorbisenc"));
+  result->allowed_audio_codecs =
+    g_list_append (result->allowed_audio_codecs,
+		   gtranscode_element_factory_find ("speexenc"));
+  result->allowed_audio_codecs =
+    g_list_append (result->allowed_audio_codecs,
+		   gtranscode_element_factory_find ("flacenc"));
+  result->allowed_audio_codecs =
+    g_list_append (result->allowed_audio_codecs,
+		   gtranscode_element_factory_find ("alsasink"));
+  result->allowed_audio_codecs =
+    g_list_append (result->allowed_audio_codecs,
+		   gtranscode_element_factory_find ("identity"));
+  result->allowed_audio_codecs =
+    g_list_append (result->allowed_audio_codecs,
+		   gtranscode_element_factory_find ("fakesink"));
+  result->allowed_video_codecs =
+    g_list_append (result->allowed_video_codecs,
+		   gtranscode_element_factory_find ("theoraenc"));
+  result->allowed_video_codecs =
+    g_list_append (result->allowed_video_codecs,
+		   gtranscode_element_factory_find ("xvimagesink"));
+  result->allowed_video_codecs =
+    g_list_append (result->allowed_video_codecs,
+		   gtranscode_element_factory_find ("aasink"));
+  return result;
 }
 
 void
-transcode (GstElementFactory * source_factory,
-	   GList * source_opts,
-	   GstElementFactory * container_factory,
-	   GList * container_opts,
-	   GstElementFactory * audio_codec_factory,
-	   GList * audio_codec_opts,
-	   GstElementFactory * video_codec_factory,
-	   GList * video_codec_opts,
-	   GstElementFactory * sink_factory, GList * sink_opts)
-{
-  GstElement *pipeline, *source, *container, *video_codec, *audio_codec,
-    *sink;
-
-  /* Set up gst elements */
-  pipeline = gst_pipeline_new ("transcoder");
-  source = gst_element_factory_create (source_factory, "source");
-  container = gst_element_factory_create (container_factory, "muxer");
-  video_codec =
-    gst_element_factory_create (video_codec_factory, "video_codec");
-  audio_codec =
-    gst_element_factory_create (audio_codec_factory, "audio_codec");
-  sink = gst_element_factory_create (sink_factory, "sink");
-
-  if (!pipeline || !source || !container || !video_codec || !audio_codec
-      || !sink)
-    {
-      g_printerr ("Could not initialise elements!\n");
-    }
-
-  g_list_foreach (source_opts, (GFunc) gtranscode_object_set, source);
-  g_list_foreach (container_opts, (GFunc) gtranscode_object_set, container);
-  g_list_foreach (video_codec_opts, (GFunc) gtranscode_object_set,
-		  video_codec);
-  g_list_foreach (audio_codec_opts, (GFunc) gtranscode_object_set,
-		  audio_codec);
-  g_list_foreach (sink_opts, (GFunc) gtranscode_object_set, sink);
-
-  /* Set up events for when it's done */
-  /* Set up progress indicator */
-  /* Start pipline */
-}
-
-void
-gtranscode_ui_add (GstElementFactory * elementfactory,
-		   GtkContainer * container)
+  gtranscode_ui_add
+  (GtranscodeElementFactory * elementfactory, GtkContainer * container)
 {
   GList *siblings;
-  GtkWidget *radio;
   GSList *radiogroup;
 #ifdef DEBUG
-  g_printf ("adding %s\n", gst_element_factory_get_longname (elementfactory));
+  g_printf ("adding %s\n", elementfactory->name);
 #endif
   siblings = gtk_container_get_children (container);
   if (g_list_length (siblings) == 0)
@@ -79,94 +84,114 @@ gtranscode_ui_add (GstElementFactory * elementfactory,
     }
   else
     {
-      radiogroup = gtk_radio_button_get_group (g_list_nth_data (siblings, 0));
+      radiogroup = gtk_radio_button_get_group (siblings->data);
     }
-  radio =
-    gtk_radio_button_new_with_label (radiogroup,
-				     gst_element_factory_get_longname
-				     (elementfactory));
-  gtk_container_add (container, radio);
-  gtk_widget_show (radio);
-}
+  elementfactory->toggle =
+    GTK_RADIO_BUTTON (gtk_radio_button_new_with_label
+		      (radiogroup, elementfactory->name));
 
-GstElementFactory *
-find_GstElementFactory (GList * factory_list,
-			GtkContainer * radiogroup_container)
-{
-  GList *siblings = gtk_container_get_children (radiogroup_container);
-  while ((siblings = g_list_next (siblings)))
-    {
-      if (gtk_toggle_button_get_active (g_list_nth_data (siblings, 0)))
-	{
-	  while ((factory_list = g_list_next (factory_list)))
-	    {
-	      if (g_str_equal
-		  (gtk_label_get_text
-		   (GTK_LABEL
-		    (gtk_bin_get_child (g_list_nth_data (siblings, 0)))),
-		   gst_element_factory_get_longname (g_list_nth_data
-						     (factory_list, 0))))
-		{
-		  return (g_list_nth_data (factory_list, 0));
-		}
-	    }
-	  factory_list = g_list_first (factory_list);
-	}
-    }
-  siblings = g_list_first (siblings);
-  return NULL;
+  gtk_container_add (container, GTK_WIDGET (elementfactory->toggle));
+  gtk_widget_show (GTK_WIDGET (elementfactory->toggle));
 }
 
 void
-transcode_button_clicked (GtkButton * button, GList * sources,
-			  GList * containers, GList * audio_codecs,
-			  GList * video_codecs, GtkBox *sources_hbox, GtkBox *containers_hbox,
-              GtkBox * audio_codecs_vbox, GtkBox * video_codecs_vbox)
+gtranscode_ui_set_signal_toggle (GtranscodeElementFactory * element_factory,
+				 gpointer data)
 {
-  GstElementFactory *source_factory, *container_factory, *audio_codec_factory,
-    *video_codec_factory, *sink_factory;
-  GList *source_opts, *container_opts, *audio_codec_opts, *video_codec_opts,
-    *sink_opts;
-  GtkContainer *box;
+  g_signal_connect (element_factory->toggle, "toggled",
+		    (GCallback) gtranscode_ui_update_toggles, NULL);
+}
 
-  box = GTK_CONTAINER( sources_hbox);
-  source_factory = find_GstElementFactory(sources,box);
+gint
+  gtranscode_element_factory_is_enabled
+  (GtranscodeElementFactory * element_factory, gpointer data)
+{
+  return
+    gtk_toggle_button_get_active
+    (GTK_TOGGLE_BUTTON (element_factory->toggle)) ? 0 : 1;
+}
+
+void
+gtranscode_ui_update_toggles (GtkToggleButton * toggle_buttion)
+{
+  GtranscodeElementFactory *container_factory =
+    g_list_find_custom (containers, NULL,
+			(GCompareFunc)
+			gtranscode_element_factory_is_enabled)->data;
+  g_list_foreach (gtk_container_get_children
+		  (GTK_CONTAINER
+		   (glade_xml_get_widget (xml, "audiocodec_vbox"))),
+		  (GFunc) gtk_widget_destroy, NULL);
+  g_list_foreach (gtk_container_get_children
+		  (GTK_CONTAINER
+		   (glade_xml_get_widget (xml, "videocodec_vbox"))),
+		  (GFunc) gtk_widget_destroy, NULL);
+  g_list_foreach (container_factory->allowed_audio_codecs,
+		  (GFunc) gtranscode_ui_add, glade_xml_get_widget (xml,
+								   "audiocodec_vbox"));
+  g_list_foreach (container_factory->allowed_video_codecs,
+		  (GFunc) gtranscode_ui_add, glade_xml_get_widget (xml,
+								   "videocodec_vbox"));
+}
+
+void
+transcode_button_clicked (GtkButton * button)
+{
+  GstElementFactory *source_factory,
+    *container_factory,
+    *audio_codec_factory, *video_codec_factory, *sink_factory;
+  GtranscodeElementFactory *container_group;
+  GList *source_opts,
+    *container_opts, *audio_codec_opts, *video_codec_opts, *sink_opts;
+  gchar *filesrcopts[] = {
+    "location",
+    "/home/david/films/Ghostbusters.avi"
+  };
+  gchar *filesinkopts[] = {
+    "location",
+    "/tmp/gb.ogg"
+  };
+  source_factory =
+    ((GtranscodeElementFactory
+      *) (g_list_find_custom (sources, NULL,
+			      (GCompareFunc)
+			      gtranscode_element_factory_is_enabled)->data))->
+    ElementFactory;
   source_opts = NULL;
-
-  box = GTK_CONTAINER( containers_hbox);
-  container_factory = find_GstElementFactory(containers,box);
+  source_opts = g_list_append (source_opts, filesrcopts);
+  container_group =
+    (GtranscodeElementFactory
+     *) (g_list_find_custom (containers, NULL,
+			     (GCompareFunc)
+			     gtranscode_element_factory_is_enabled)->data);
+  container_factory = container_group->ElementFactory;
   container_opts = NULL;
-
-  box = GTK_CONTAINER( audio_codecs_vbox);
-  audio_codec_factory = find_GstElementFactory(audio_codecs,box);
+  audio_codec_factory =
+    ((GtranscodeElementFactory
+      *) (g_list_find_custom (container_group->allowed_audio_codecs, NULL,
+			      (GCompareFunc)
+			      gtranscode_element_factory_is_enabled)->data))->
+    ElementFactory;
   audio_codec_opts = NULL;
-
-  box = GTK_CONTAINER( video_codecs_vbox);
-  video_codec_factory = find_GstElementFactory(video_codecs,box);
+  video_codec_factory =
+    ((GtranscodeElementFactory
+      *) (g_list_find_custom (container_group->allowed_video_codecs, NULL,
+			      (GCompareFunc)
+			      gtranscode_element_factory_is_enabled)->data))->
+    ElementFactory;
   video_codec_opts = NULL;
-
-  sink_factory = gst_element_factory_find ( "filesink");
+  sink_factory = gst_element_factory_find ("filesink");
   sink_opts = NULL;
-
-  transcode (source_factory,
-	   source_opts,
-	   container_factory,
-	   container_opts,
-	   audio_codec_factory,
-	   audio_codec_opts,
-	   video_codec_factory,
-	   video_codec_opts,
-	   sink_factory,
-	   sink_opts);
+  sink_opts = g_list_append (sink_opts, filesinkopts);
+  transcode (source_factory, source_opts, container_factory, container_opts,
+	     audio_codec_factory, audio_codec_opts, video_codec_factory,
+	     video_codec_opts, sink_factory, sink_opts);
 }
 
 int
 main (int argc, char *argv[])
 {
   GtkWidget *window1;
-  GladeXML *xml;
-  GList *sources = NULL, *containers = NULL, *audio_codecs =
-    NULL, *video_codecs = NULL;
   /*GstRegistry *registry; */
 #ifdef ENABLE_NLS
   bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
@@ -183,33 +208,31 @@ main (int argc, char *argv[])
   glade_xml_signal_autoconnect (xml);
   window1 = glade_xml_get_widget (xml, "gtranscode_app");
   gtk_widget_show (window1);
+
   /* Detect Gstreamer elements avilable */
-/*
-	registry = gst_register_get_default();
-	containers = gst_registry_feature_filter(registry, gstranscode_feature_is_container, FALSE, NULL);
-	video_codecs = gst_registry_feature_filter(registry, gstranscode_feature_is_video_codec, FALSE, NULL);
-	audio_codecs = gst_registry_feature_filter(registry, gstranscode_feature_is_audio_codec, FALSE, NULL);
-*/
-  sources = g_list_append (sources, gst_element_factory_find ("filesrc"));
+  sources =
+    g_list_append (sources, gtranscode_element_factory_find ("filesrc"));
   containers =
-    g_list_append (containers, gst_element_factory_find ("oggmux"));
-  video_codecs =
-    g_list_append (video_codecs, gst_element_factory_find ("theoraenc"));
-  audio_codecs =
-    g_list_append (audio_codecs, gst_element_factory_find ("vorbisenc"));
-  audio_codecs =
-    g_list_append (audio_codecs, gst_element_factory_find ("speexenc"));
-  audio_codecs =
-    g_list_append (audio_codecs, gst_element_factory_find ("flacenc"));
+    g_list_append
+    (containers, gtranscode_element_factory_find_with_children ("oggmux"));
+
   /* Set up ui for Gstreamer elements available */
-  g_list_foreach (sources, (GFunc) gtranscode_ui_add,
-		  glade_xml_get_widget (xml, "sources_hbox"));
+  g_list_foreach
+    (sources,
+     (GFunc) gtranscode_ui_add, glade_xml_get_widget (xml, "sources_hbox"));
   g_list_foreach (containers, (GFunc) gtranscode_ui_add,
 		  glade_xml_get_widget (xml, "container_hbox"));
-  g_list_foreach (video_codecs, (GFunc) gtranscode_ui_add,
-		  glade_xml_get_widget (xml, "videocodec_vbox"));
-  g_list_foreach (audio_codecs, (GFunc) gtranscode_ui_add,
-		  glade_xml_get_widget (xml, "audiocodec_vbox"));
+  g_list_foreach (containers, (GFunc) gtranscode_ui_set_signal_toggle,
+		  glade_xml_get_widget (xml, "container_hbox"));
+  g_list_foreach (gtk_container_get_children
+		  (GTK_CONTAINER
+		   (glade_xml_get_widget (xml, "container_hbox"))),
+		  (GFunc) gtranscode_ui_update_toggles, NULL);
+
   gtk_main ();
+  #ifdef DEBUG
+  g_printf ("Printing pipline.\n");
+  gst_xml_write_file (pipeline, g_fopen ("/tmp/out.xml", "w"));
+#endif
   return 0;
 }
